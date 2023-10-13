060fbf161fad9287e397d46360684804ced5f27f
KDanielRosas <k.daniel.rosas99@gmail.com>
2023-10-13 15:43:12 -0600

    Add project files.

---
+++ BL/Credito.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Credito
+    {
+        public static ML.Result GetAllEF()
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL_EF.CreditosEntities context = new DL_EF.CreditosEntities())
+                {
+                    var resultQuery = context.CreditoGetAll().ToList();
+
+                    if (resultQuery != null)
+                    {
+                        result.Objects = new List<object>();
+                        foreach (var obj in resultQuery)
+                        {
+                            ML.Credito credito = new ML.Credito();
+
+                            credito.NumeroCredito = obj.NumeroCredito;
+                            credito.ImporteCredito = (decimal)obj.ImporteCredito;
+                            credito.PLazo = (int)obj.Plazo;
+                            credito.TasaAnual = (decimal)obj.TasaAnual;
+
+
+                            result.Objects.Add(credito);
+                            result.Correct = true;
+                        }
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+
+        public static ML.Result DeleteEF(ML.Credito credito)
+        {
+            ML.Result result = new ML.Result();
+
+           
+            try
+            {
+                using (DL_EF.CreditosEntities context = new DL_EF.CreditosEntities())
+                {
+                    var updateResult = context.CreditoDelete(credito.NumeroCredito);
+                    if (updateResult >= 1)
+                    {
+                        result.Correct = true;
+
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ PL/Controllers/CreditoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class CreditoController : Controller
+    {
+        // GET: Credito
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            ML.Credito credito = new ML.Credito();
+            credito.Creditos = new List<object>();
+            ML.Result result = BL.Credito.GetAllEF();
+
+
+            if (result.Correct)
+            {
+                credito.Creditos = result.Objects;
+            }
+            else
+            {
+                ViewBag.Message = result.ErrorMessage; 
+            }
+            return View(credito);
+        }
+
+        public ActionResult Delete( int credito)
+        {
+            ML.Credito creditos = new ML.Credito();
+
+            creditos.NumeroCredito = credito;
+
+            ML.Result result = BL.Credito.DeleteEF(creditos);
+            
+
+            if (result.Correct)
+            {
+                ViewBag.Message = "Se ha eliminado correctamente el usuario";
+            }
+
+            else
+            {
+                ViewBag.Message = "no se puede eliminar correctemnte el usuario , Error: " + result.ErrorMessage;
+            }
+
+            return PartialView("Modal");
+        }
+    }
+}
